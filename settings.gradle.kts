4655d51337a84438d1aa5c2e4a6c03e1e380edd0
Dominique Dymke <dominique@dymke.net>
2023-04-06 20:46:17 +1000

    got rid of deprecation warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,15 +2,12 @@ rootProject.name = "hamal"
 
 include("application")
 
-include("lib")
-
 include(":lib:ddd")
 include(":lib:domain")
 include(":lib:domain-value-object")
 include(":lib:meta")
 include(":lib:util")
 
-include(":module")
 include(":module:launchpad")
 include(":module:launchpad:application")
 include(":module:launchpad:core")
