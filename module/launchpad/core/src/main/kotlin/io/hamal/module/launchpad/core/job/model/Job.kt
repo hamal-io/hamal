adc4b424a7b8d9f13bcdd3c09e99b570f729843d
Dominique Dymke <dominique@dymke.net>
2023-04-27 20:46:04 +1000

    removed region id from model

--- module/launchpad/core/src/main/kotlin/io/hamal/module/launchpad/core/job/model/Job.kt
+++ module/launchpad/core/src/main/kotlin/io/hamal/module/launchpad/core/job/model/Job.kt
@@ -4,78 +4,64 @@ import io.hamal.lib.domain.vo.JobDefinitionId
 import io.hamal.lib.domain.vo.JobId
 import io.hamal.lib.domain.vo.JobState
 import io.hamal.lib.domain.vo.JobState.*
-import io.hamal.lib.domain.vo.RegionId
 
 sealed class Job(
     val id: JobId,
     val state: JobState,
-    val regionId: RegionId,
     val definitionId: JobDefinitionId
 ) {
 
     class PlannedJob(
         id: JobId,
-        regionId: RegionId,
         definitionId: JobDefinitionId
     ) : Job(
         id = id,
         state = Planned,
-        regionId = regionId,
         definitionId = definitionId
     )
 
     class ScheduledJob(
         id: JobId,
-        regionId: RegionId,
         definitionId: JobDefinitionId
     ) : Job(
         id = id,
         state = Scheduled,
-        regionId = regionId,
         definitionId = definitionId
     )
 
     class StartedJob(
         id: JobId,
-        regionId: RegionId,
         definitionId: JobDefinitionId
     ) : Job(
         id = id,
         state = Started,
-        regionId = regionId,
         definitionId = definitionId
     )
 
     class CompletedJob(
         id: JobId,
-        regionId: RegionId,
         definitionId: JobDefinitionId
     ) : Job(
         id = id,
         state = Completed,
-        regionId = regionId,
         definitionId = definitionId
     )
 
     class FailedJob(
         id: JobId,
-        regionId: RegionId,
         definitionId: JobDefinitionId
     ) : Job(
         id = id,
         state = Failed,
-        regionId = regionId,
         definitionId = definitionId
     )
 
     class TerminalFailedJob(
         id: JobId,
-        regionId: RegionId,
         definitionId: JobDefinitionId
     ) : Job(
         id = id,
         state = TerminalFailed,
-        regionId = regionId,
         definitionId = definitionId
     )
 }
