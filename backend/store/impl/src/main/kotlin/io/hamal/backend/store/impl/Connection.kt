dd90e56b48b607e49a52649b0ee8876f3a7bfa80
Dominique Dymke <dominique@dymke.net>
2023-05-03 07:49:43 +1000

    adds executeUpdate to Connection

--- backend/store/impl/src/main/kotlin/io/hamal/backend/store/impl/Connection.kt
+++ backend/store/impl/src/main/kotlin/io/hamal/backend/store/impl/Connection.kt
@@ -1,16 +1,58 @@
 package io.hamal.backend.store.impl
 
 import io.hamal.backend.store.impl.DefaultNamedPreparedStatement.Companion.prepare
+import io.hamal.lib.KeyedOnce
+import io.hamal.lib.RequestId
+import io.hamal.lib.util.SnowflakeId
+import io.hamal.lib.vo.base.DomainId
 import java.sql.DriverManager
+import java.time.Instant
 
-class NamedPreparedStatementDelegate constructor(
+class NamedPreparedStatementDelegate(
     internal val delegate: NamedPreparedStatement<*>
 ) {
+    operator fun set(param: String, value: Boolean): NamedPreparedStatementDelegate {
+        delegate[param] = value
+        return this
+    }
+
+    operator fun set(param: String, value: Int): NamedPreparedStatementDelegate {
+        delegate[param] = value
+        return this
+    }
+
+    operator fun set(param: String, value: Long): NamedPreparedStatementDelegate {
+        delegate[param] = value
+        return this
+    }
+
+    operator fun set(param: String, value: Instant): NamedPreparedStatementDelegate {
+        delegate[param] = value
+        return this
+    }
+
+    operator fun set(param: String, value: SnowflakeId): NamedPreparedStatementDelegate {
+        delegate[param] = value
+        return this
+    }
+
+    operator fun set(param: String, value: DomainId): NamedPreparedStatementDelegate {
+        delegate[param] = value
+        return this
+    }
+
+    operator fun set(param: String, value: RequestId): NamedPreparedStatementDelegate {
+        delegate[param] = value
+        return this
+    }
+
     operator fun set(param: String, value: String): NamedPreparedStatementDelegate {
         delegate[param] = value
         return this
     }
+
 }
+
 
 interface Connection : AutoCloseable {
     val isOpen: Boolean
@@ -18,12 +60,21 @@ interface Connection : AutoCloseable {
     fun prepare(sql: String): NamedPreparedStatement<*>
     fun execute(sql: String)
     fun execute(sql: String, block: NamedPreparedStatementDelegate.() -> NamedPreparedStatementDelegate)
-}
+    fun executeUpdate(sql: String): Int
+    fun executeUpdate(sql: String, block: NamedPreparedStatementDelegate.() -> NamedPreparedStatementDelegate): Int
 
+    //executeQuery
+//    fun <T : Any> executeQuery(
+//        sql: String,
+//        block: NamedPreparedStatementResultSetDelegate.() -> NamedPreparedStatementResultSetDelegate
+//    ): T
+}
 
 class DefaultConnection(url: String) : Connection {
 
     val delegate: java.sql.Connection
+
+    val statements = KeyedOnce.default<String, NamedPreparedStatement<*>>()
 
     init {
         delegate = DriverManager.getConnection(url)
@@ -31,19 +82,52 @@ class DefaultConnection(url: String) : Connection {
 
     override val isOpen: Boolean get() = !delegate.isClosed
     override val isClosed: Boolean get() = delegate.isClosed
-    override fun prepare(sql: String) = delegate.prepare(sql)
-    override fun execute(sql: String) {
-        delegate.createStatement().use {
-            it.execute(sql)
+    override fun prepare(sql: String): NamedPreparedStatement<*> {
+        return statements(sql) {
+            delegate.prepare(sql)
+        }.apply {
+            this.clearParameter()
         }
     }
 
-    override fun execute(sql: String, block: NamedPreparedStatementDelegate.() -> NamedPreparedStatementDelegate) {
+    override fun execute(sql: String) {
+        prepare(sql).use {
+            it.execute()
+        }
+    }
+
+    override fun execute(
+        sql: String,
+        block: NamedPreparedStatementDelegate.() -> NamedPreparedStatementDelegate
+    ) {
         prepare(sql).use {
             block(NamedPreparedStatementDelegate(it))
             it.execute()
         }
     }
+
+    override fun executeUpdate(sql: String): Int {
+        return prepare(sql).use {
+            it.executeUpdate()
+        }
+    }
+
+    override fun executeUpdate(
+        sql: String,
+        block: NamedPreparedStatementDelegate.() -> NamedPreparedStatementDelegate
+    ): Int {
+        return prepare(sql).use {
+            block(NamedPreparedStatementDelegate(it))
+            it.executeUpdate()
+        }
+    }
+
+//    override fun <T : Any> executeQuery(
+//        sql: String,
+//        block: NamedPreparedStatementResultSetDelegate.() -> NamedPreparedStatementResultSetDelegate
+//    ): T {
+//        TODO("Not yet implemented")
+//    }
 
     override fun close() {
         delegate.close()
--- backend/store/impl/src/testIntegration/kotlin/io/hamal/backend/store/impl/ConnectionIT.kt
+++ backend/store/impl/src/testIntegration/kotlin/io/hamal/backend/store/impl/ConnectionIT.kt
@@ -1,12 +1,17 @@
 package io.hamal.backend.store.impl
 
+import io.hamal.lib.RequestId
+import io.hamal.lib.util.SnowflakeId
+import io.hamal.lib.vo.base.DomainId
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.nio.file.Files
+import java.time.Instant
 
 class DefaultConnectionIT {
 
@@ -14,23 +19,114 @@ class DefaultConnectionIT {
     @DisplayName("prepare()")
     inner class PrepareTest {
         @Test
-        fun implementMe() {
-            TODO()
+        fun `Statement gets cached and can be prepared multiple time`() {
+            val result = testInstance.prepare("INSERT INTO some_table(value) VALUES (:some_value)")
+            result["some_value"] = 1234
+            result.execute()
+
+            val anotherResult = testInstance.prepare("INSERT INTO some_table(value) VALUES (:some_value)")
+            anotherResult["some_value"] = 5432
+            anotherResult.execute()
+
+            assertSame(result, anotherResult)
+
+            testInstance.prepare("SELECT COUNT(*) as count FROM some_table").executeQuery()
+                .use {
+                    assertThat(it.getInt("count"), equalTo(2))
+                }
+        }
+
+        private val testInstance = DefaultConnection("jdbc:sqlite:${Files.createTempDirectory("execute")}/db.sqlite")
+
+        init {
+            testInstance.execute("""PRAGMA journal_mode = wal;""")
+            testInstance.execute("""PRAGMA locking_mode = exclusive;""")
+            testInstance.execute("""PRAGMA temp_store = memory;""")
+            testInstance.execute("""PRAGMA synchronous = off;""")
+
+            testInstance.execute("""CREATE TABLE some_table(value INT NOT NULL)""")
         }
     }
 
     @Nested
     @DisplayName("execute()")
     inner class ExecuteTest {
-        //FIXME missing parameter
-        //FIXME wrong parameter name
+        @Test
+        fun `Named parameter is missing`() {
+            val exception = assertThrows<IllegalArgumentException> {
+                testInstance.execute("INSERT INTO string_table(value) VALUES(:some_value)")
+            }
+            assertThat(
+                exception.message,
+                containsString("Expected all named parameters to be set, but [some_value] are missing")
+            )
+        }
 
         @Test
-        fun `Execute with named parameter of type string`() {
+        fun `With named parameter of type boolean`() {
+            testInstance.execute("INSERT INTO boolean_table(value) VALUES(:some_value)") {
+                set("some_value", true)
+            }
+            verifyIsOne("SELECT COUNT(*) FROM boolean_table WHERE value = true")
+        }
+
+        @Test
+        fun `With named parameter of type int`() {
+            testInstance.execute("INSERT INTO int_table(value) VALUES(:some_value)") {
+                set("some_value", 1337)
+            }
+            verifyIsOne("SELECT COUNT(*) FROM int_table WHERE value = 1337")
+        }
+
+        @Test
+        fun `With named parameter of type long`() {
+            testInstance.execute("INSERT INTO long_table(value) VALUES(:some_value)") {
+                set("some_value", 12345678901234567L)
+            }
+            verifyIsOne("SELECT COUNT(*) FROM long_table WHERE value = 12345678901234567")
+        }
+
+        @Test
+        fun `With named parameter of type instant`() {
+            testInstance.execute("INSERT INTO instant_table(value) VALUES(:some_value)") {
+                set("some_value", Instant.ofEpochMilli(12345678))
+            }
+            verifyIsOne("SELECT COUNT(*) FROM instant_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type snowflake_id`() {
+            testInstance.execute("INSERT INTO snowflake_id_table(value) VALUES(:some_value)") {
+                set("some_value", SnowflakeId(12345678))
+            }
+            verifyIsOne("SELECT COUNT(*) FROM snowflake_id_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type domain_id`() {
+            class TestDomainId(override val value: SnowflakeId) : DomainId() {
+                constructor(value: Int) : this(SnowflakeId(value.toLong()))
+            }
+
+            testInstance.execute("INSERT INTO domain_id_table(value) VALUES(:some_value)") {
+                set("some_value", TestDomainId(12345678))
+            }
+            verifyIsOne("SELECT COUNT(*) FROM domain_id_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type request_id`() {
+            testInstance.execute("INSERT INTO request_id_table(value) VALUES(:some_value)") {
+                set("some_value", RequestId(12345678))
+            }
+            verifyIsOne("SELECT COUNT(*) FROM request_id_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type string`() {
             testInstance.execute("INSERT INTO string_table(value) VALUES(:some_value)") {
                 set("some_value", "ThisHamalConnectionRockz")
             }
-
             verifyIsOne("SELECT COUNT(*) FROM string_table WHERE value = 'ThisHamalConnectionRockz'")
         }
 
@@ -58,6 +154,157 @@ class DefaultConnectionIT {
                     assertThat(it.getInt(1), equalTo(1))
                 }
             }
+        }
+    }
+
+    @Nested
+    @DisplayName("executeUpdate()")
+    inner class ExecuteUpdateTest {
+        @Test
+        fun `Named parameter is missing`() {
+            val exception = assertThrows<IllegalArgumentException> {
+                testInstance.executeUpdate("INSERT INTO string_table(value) VALUES(:some_value)")
+            }
+            assertThat(
+                exception.message,
+                containsString("Expected all named parameters to be set, but [some_value] are missing")
+            )
+        }
+
+        @Test
+        fun `With named parameter of type boolean`() {
+            val result = testInstance.executeUpdate("INSERT INTO boolean_table(value) VALUES(:some_value)") {
+                set("some_value", true)
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM boolean_table WHERE value = true")
+        }
+
+        @Test
+        fun `With named parameter of type int`() {
+            val result = testInstance.executeUpdate("INSERT INTO int_table(value) VALUES(:some_value)") {
+                set("some_value", 1337)
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM int_table WHERE value = 1337")
+        }
+
+        @Test
+        fun `With named parameter of type long`() {
+            val result = testInstance.executeUpdate("INSERT INTO long_table(value) VALUES(:some_value)") {
+                set("some_value", 12345678901234567L)
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM long_table WHERE value = 12345678901234567")
+        }
+
+        @Test
+        fun `With named parameter of type instant`() {
+            val result = testInstance.executeUpdate("INSERT INTO instant_table(value) VALUES(:some_value)") {
+                set("some_value", Instant.ofEpochMilli(12345678))
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM instant_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type snowflake_id`() {
+            val result = testInstance.executeUpdate("INSERT INTO snowflake_id_table(value) VALUES(:some_value)") {
+                set("some_value", SnowflakeId(12345678))
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM snowflake_id_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type domain_id`() {
+            class TestDomainId(override val value: SnowflakeId) : DomainId() {
+                constructor(value: Int) : this(SnowflakeId(value.toLong()))
+            }
+
+            val result = testInstance.executeUpdate("INSERT INTO domain_id_table(value) VALUES(:some_value)") {
+                set("some_value", TestDomainId(12345678))
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM domain_id_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type request_id`() {
+            val result = testInstance.executeUpdate("INSERT INTO request_id_table(value) VALUES(:some_value)") {
+                set("some_value", RequestId(12345678))
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM request_id_table WHERE value = 12345678")
+        }
+
+        @Test
+        fun `With named parameter of type string`() {
+            val result = testInstance.executeUpdate("INSERT INTO string_table(value) VALUES(:some_value)") {
+                set("some_value", "ThisHamalConnectionRockz")
+            }
+            assertThat(result, equalTo(1))
+            verifyIsOne("SELECT COUNT(*) FROM string_table WHERE value = 'ThisHamalConnectionRockz'")
+        }
+
+        private val testInstance =
+            DefaultConnection("jdbc:sqlite:${Files.createTempDirectory("executeUpdate")}/db.sqlite")
+
+        init {
+            testInstance.execute("""PRAGMA journal_mode = wal;""")
+            testInstance.execute("""PRAGMA locking_mode = exclusive;""")
+            testInstance.execute("""PRAGMA temp_store = memory;""")
+            testInstance.execute("""PRAGMA synchronous = off;""")
+
+            testInstance.execute("""CREATE TABLE boolean_table(value BOOLEAN NOT NULL)""")
+            testInstance.execute("""CREATE TABLE int_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE long_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE string_table(value TEXT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE instant_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE snowflake_id_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE domain_id_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE request_id_table(value INT NOT NULL)""")
+        }
+
+        private fun verifyIsOne(query: String) {
+            testInstance.delegate.createStatement().use { stmt ->
+                stmt.executeQuery(query).use {
+                    assertThat(it.getInt(1), equalTo(1))
+                }
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("executeQuery")
+    inner class ExecuteQueryTest {
+
+//        fun `With named parameter and result set of type int`() {
+//            testInstance.executeQuery<Int>("") {
+//                this[""] = 123
+//                returns {
+//                    123
+//                }
+//            }
+//        }
+
+        private val testInstance =
+            DefaultConnection("jdbc:sqlite:${Files.createTempDirectory("executeUpdate")}/db.sqlite")
+
+        init {
+            testInstance.execute("""PRAGMA journal_mode = wal;""")
+            testInstance.execute("""PRAGMA locking_mode = exclusive;""")
+            testInstance.execute("""PRAGMA temp_store = memory;""")
+            testInstance.execute("""PRAGMA synchronous = off;""")
+
+            testInstance.execute("""CREATE TABLE boolean_table(value BOOLEAN NOT NULL)""")
+            testInstance.execute("""CREATE TABLE int_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE long_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE string_table(value TEXT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE instant_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE snowflake_id_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE domain_id_table(value INT NOT NULL)""")
+            testInstance.execute("""CREATE TABLE request_id_table(value INT NOT NULL)""")
         }
     }
 
