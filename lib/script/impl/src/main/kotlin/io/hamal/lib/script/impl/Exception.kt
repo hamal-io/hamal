5645efe6e4de839c012dfea51bd909551f571cb5
Dominique Dymke <dominique@dymke.net>
2023-05-14 15:07:07 +1000

    some clean up

--- lib/script/impl/src/main/kotlin/io/hamal/lib/script/impl/Exception.kt
+++ lib/script/impl/src/main/kotlin/io/hamal/lib/script/impl/Exception.kt
@@ -6,11 +6,11 @@ sealed class ScriptException(message: String? = null, cause: Throwable? = null) 
     constructor(cause: Throwable) : this(null, cause)
 }
 
-class ScriptParseException(message: String) : io.hamal.lib.script.impl.ScriptException(message)
+class ScriptParseException(message: String) : ScriptException(message)
 
-class ScriptEvaluationException(val error: ErrorValue) : io.hamal.lib.script.impl.ScriptException(message = error.toString())
+class ScriptEvaluationException(val error: ErrorValue) : ScriptException(message = error.toString())
 
-class ForeignFunctionInvocationException(message: String? = null, cause: Throwable? = null) :
-    io.hamal.lib.script.impl.ScriptException(message, cause) {
+class NativeFunctionInvocationException(message: String? = null, cause: Throwable? = null) :
+    ScriptException(message, cause) {
     constructor(cause: Throwable) : this(null, cause)
 }
--- lib/script/impl/src/main/kotlin/io/hamal/lib/script/impl/Sandbox.kt
+++ lib/script/impl/src/main/kotlin/io/hamal/lib/script/impl/Sandbox.kt
@@ -2,20 +2,23 @@ package io.hamal.lib.script.impl
 
 import io.hamal.lib.script.api.Sandbox
 import io.hamal.lib.script.api.value.Value
+import io.hamal.lib.script.impl.ast.parse
 import io.hamal.lib.script.impl.interpreter.Environment
 import io.hamal.lib.script.impl.interpreter.Interpreter
 import io.hamal.lib.script.impl.token.tokenize
 
-class DefaultSandbox(val env: Environment) : Sandbox {
+class DefaultSandbox(
+    private val env: Environment
+) : Sandbox {
 
     private val interpreter = Interpreter.DefaultImpl
 
     override fun eval(code: String): Value {
         val tokens = tokenize(code)
-        val statements = io.hamal.lib.script.impl.ast.parse(tokens)
+        val statements = parse(tokens)
         try {
             return interpreter.run(statements, env)
-        } catch (e: io.hamal.lib.script.impl.ScriptEvaluationException) {
+        } catch (e: ScriptEvaluationException) {
             return e.error
         } catch (t: Throwable) {
             throw t
