1f04b8489b7827589bdc555ba462f429be40b6f3
Dominique Dymke <dominique@dymke.net>
2023-07-06 07:30:02 +1000

    clean up

--- bootstrap/src/integrationTest/kotlin/io/hamal/bootstrap/SqliteHamalTest.kt
+++ bootstrap/src/integrationTest/kotlin/io/hamal/bootstrap/SqliteHamalTest.kt
@@ -1,13 +1,6 @@
 package io.hamal.bootstrap
 
-import io.hamal.agent.AgentConfig
-import io.hamal.backend.instance.BackendConfig
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
 @ActiveProfiles("memory")
