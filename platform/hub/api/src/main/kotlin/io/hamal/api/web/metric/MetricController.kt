e0ba9fb2e1a25fb3f3f80f065e34828c3f145756
Dominique Dymke <dominique@dymke.net>
2023-09-19 17:44:57 +0700

    have metric controller in api as well

---
+++ platform/hub/api/src/main/kotlin/io/hamal/api/web/metric/MetricController.kt
@@ -0,0 +1,27 @@
+package io.hamal.admin.web.metric
+
+import io.hamal.core.service.MetricService
+import io.hamal.lib.sdk.admin.AdminMetrics
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class MetricController(
+    private val metricService: MetricService
+) {
+    @GetMapping("/v1/metrics", produces = ["application/json"])
+    fun json(): ResponseEntity<AdminMetrics> {
+        val result = metricService.get()
+        return ResponseEntity.ok(
+            AdminMetrics(result.getTime(), result.getMap())
+        )
+    }
+
+    @PostMapping("/v1/metrics/clear")
+    fun clear(){
+        metricService.clear()
+    }
+}
