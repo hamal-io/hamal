4fa248f684960a7ee37c4c7437f05dad8a115604
Dominique Dymke <dominique@dymke.net>
2023-10-01 06:28:27 +0700

    empty antd page

---
+++ project/fn_guru/src/main/resources/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css'
+import { Button } from 'antd';
+
+const App = () => (
+    <div className="App">
+        <Button type="primary">Button 1</Button>
+    </div>
+);
+
+export default App;
