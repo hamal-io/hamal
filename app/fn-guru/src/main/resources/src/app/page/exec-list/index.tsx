791db17309e8cc7daa1631e55b2bd99b3752f2fc
Dominique Dymke <dominique@dymke.net>
2023-10-07 08:15:22 +0700

    adds placeholder for func list and exec list

--- app/fn-guru/src/main/resources/src/app/page/exec-list/index.tsx
+++ app/fn-guru/src/main/resources/src/app/page/exec-list/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const DashboardPage: React.FC = () => (
+const ExecListPage: React.FC = () => (
     <main className="flex-1 w-full mx-auto p-4 text-lg h-full shadow-lg bg-gray-100">
         <div className="flex flex-col items-center justify-center">
-            <p> Dashboard </p>
+            <p> Executions </p>
         </div>
     </main>
 );
 
-export default DashboardPage
+export default ExecListPage
 
