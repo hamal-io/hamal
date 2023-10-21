55ac94115077f7bf198a129d4120c3cb543714fa
Dominique Dymke <dominique@dymke.net>
2023-10-21 08:25:19 +0800

    allows trailing slash

--- platform/lib/web3/src/main/kotlin/io/hamal/lib/web3/Playground.kt
+++ platform/lib/web3/src/main/kotlin/io/hamal/lib/web3/Playground.kt
@@ -2,14 +2,13 @@ package io.hamal.lib.web3
 
 import io.hamal.lib.http.HttpTemplate
 import io.hamal.lib.web3.Erc20.decimals
+import io.hamal.lib.web3.US.getReserves
 import io.hamal.lib.web3.eth.EthBatchService
-import io.hamal.lib.web3.eth.abi.EthFunction
-import io.hamal.lib.web3.eth.abi.EthInputTuple0
-import io.hamal.lib.web3.eth.abi.EthOutput
-import io.hamal.lib.web3.eth.abi.EthOutputTuple1
+import io.hamal.lib.web3.eth.abi.*
 import io.hamal.lib.web3.eth.abi.type.EthAddress
 import io.hamal.lib.web3.eth.abi.type.EthPrefixedHexString
 import io.hamal.lib.web3.eth.abi.type.EthUint64
+import io.hamal.lib.web3.eth.domain.EthCallResponse
 import io.hamal.lib.web3.eth.http.EthHttpBatchService
 
 
@@ -40,70 +39,36 @@ object Erc20 {
     )
 }
 
+object US{
+    val getReserves = EthFunction(
+        "getReserves",
+        EthInput.Tuple0(),
+        EthOutput.Tuple3(
+            EthOutput.Uint112("_reserve0"),
+            EthOutput.Uint112("_reserve1"),
+            EthOutput.Uint32("_blockTimestampLast")
+        )
+    )
+}
+
 
 fun main() {
     val srv = EthHttpBatchService(
-        HttpTemplate("http://localhost:8000")
+        HttpTemplate("http://localhost:10001")
     )
-
 
     val response = srv
         .call(
         EthBatchService.EthCallRequest(
             to = EthAddress(EthPrefixedHexString("0x570febdf89c07f256c75686caca215289bb11cfc")),
             data = EthPrefixedHexString("0x0902f1ac"),
-            blockNumber = EthUint64(12040752L)
+            blockNumber = EthUint64(12040753L)
         )
-    )
-//        .callFunction(
-//            to = EthAddress(EthPrefixedHexString("0x570febdf89c07f256c75686caca215289bb11cfc")),
-//            function = getReserves,
-//            blockNumber = EthUint64(12040752L)
-//        )
-//        .execute().first()
-//
-//
-//    require(response is EthCallResp)
-//    println(response)
-//
-//    val x = getReserves.outputs.decodeToMap(response.result)
-//    System.out.println(x["_reserve0"])
-//    System.out.println(x["_reserve1"])
+    ).execute()
 
-//    val response = srv
-////        .call(
-////        EthBatchService.EthCallRequest(
-////            to = EthAddress(EthPrefixedHexString("0x570febdf89c07f256c75686caca215289bb11cfc")),
-////            data = EthPrefixedHexString("0x0902f1ac"),
-////            blockNumber = EthUint64(12040752L)
-////        )
-////    )
-////        .callFunction(
-////            to = EthAddress(EthPrefixedHexString("0x570febdf89c07f256c75686caca215289bb11cfc")),
-////            function = getReserves,
-////            blockNumber = EthUint64(12040752L)
-////        )
-////        .callFunction(
-////            to = EthAddress(EthPrefixedHexString("0x570febdf89c07f256c75686caca215289bb11cfc")),
-////            function = decimals,
-////            blockNumber = EthUint64(12040752L)
-////        )
-//        .call(
-//            to = EthAddress(EthPrefixedHexString("0x570febdf89c07f256c75686caca215289bb11cfc")),
-//            function = decimals,
-//            blockNumber = EthUint64(12040752L)
-//        )
-//        .execute().first()
-//
-//    println(response)
-//
-//
-//    require(response is EthCallResponse)
-//
-//    val x = decimals.outputs.decodeToMap(response.result)
-//
-//    println(x)
+    val x = getReserves.outputs.decodeToMap((response[0] as EthCallResponse).result)
+    System.out.println(x["_reserve0"])
+    System.out.println(x["_reserve1"])
 
     println(decimals.signature.encoded.toPrefixedHexString())
-
 }
