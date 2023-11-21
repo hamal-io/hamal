5293f76e67f99f7f2e6ad29e7441040e2017caf4
Dominique Dymke <dominique@dymke.net>
2023-11-21 00:00:00 +0000

    104 - fixes failing tests

--- platform/backend/repository/testbed/src/test/kotlin/io/hamal/repository/fixture/SqliteFixture.kt
+++ platform/backend/repository/testbed/src/test/kotlin/io/hamal/repository/fixture/SqliteFixture.kt
@@ -27,76 +27,76 @@ object SqliteFixture : BaseTestFixture {
     @Suppress("UNCHECKED_CAST")
     override fun <REPO : Any> provideImplementation(interfaceClass: KClass<out REPO>): REPO = when (interfaceClass) {
         AccountRepository::class -> SqliteAccountRepository(
-            SqliteAccountRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_account_test"))
+            SqliteAccountRepository.Config(createTempDirectory("sqlite_account_test"))
         ) as REPO
 
         AuthRepository::class -> SqliteAuthRepository(
-            SqliteAuthRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_auth_test"))
+            SqliteAuthRepository.Config(createTempDirectory("sqlite_auth_test"))
         ) as REPO
 
         BlueprintRepository::class -> SqliteBlueprintRepository(
-            SqliteBlueprintRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_blueprint_test"))
+            SqliteBlueprintRepository.Config(createTempDirectory("sqlite_blueprint_test"))
         ) as REPO
 
         BrokerConsumersRepository::class -> SqliteBrokerConsumersRepository(
-            SqliteBrokerConsumers(createTempDirectory("/tmp/hamal_test/sqlite_broker_consumers_test"))
+            SqliteBrokerConsumers(createTempDirectory("sqlite_broker_consumers_test"))
         ) as REPO
 
         BrokerRepository::class -> SqliteBrokerRepository(
-            SqliteBroker(createTempDirectory("/tmp/hamal_test/sqlite_broker_test"))
+            SqliteBroker(createTempDirectory("sqlite_broker_test"))
         ) as REPO
 
         BrokerTopicsRepository::class -> SqliteBrokerTopicsRepository(
-            SqliteBrokerTopics(createTempDirectory("/tmp/hamal_test/sqlite_broker_topics_test"))
+            SqliteBrokerTopics(createTempDirectory("sqlite_broker_topics_test"))
         ) as REPO
 
         CodeRepository::class -> SqliteCodeRepository(
-            SqliteCodeRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_code_test"))
+            SqliteCodeRepository.Config(createTempDirectory("sqlite_code_test"))
         ) as REPO
 
         ExecRepository::class -> SqliteExecRepository(
-            SqliteExecRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_exec_test"))
+            SqliteExecRepository.Config(createTempDirectory("sqlite_exec_test"))
         ) as REPO
 
         ExtensionRepository::class -> SqliteExtensionRepository(
-            SqliteExtensionRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_extension_test"))
+            SqliteExtensionRepository.Config(createTempDirectory("sqlite_extension_test"))
         ) as REPO
 
         FuncRepository::class -> SqliteFuncRepository(
-            SqliteFuncRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_func_test"))
+            SqliteFuncRepository.Config(createTempDirectory("sqlite_func_test"))
         ) as REPO
 
         GroupRepository::class -> SqliteGroupRepository(
-            SqliteGroupRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_group_test"))
+            SqliteGroupRepository.Config(createTempDirectory("sqlite_group_test"))
         ) as REPO
 
         HookRepository::class -> SqliteHookRepository(
-            SqliteHookRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_hook_test"))
+            SqliteHookRepository.Config(createTempDirectory("sqlite_hook_test"))
         ) as REPO
 
         FlowRepository::class -> SqliteFlowRepository(
-            SqliteFlowRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_flow_test"))
+            SqliteFlowRepository.Config(createTempDirectory("sqlite_flow_test"))
         ) as REPO
 
         SegmentRepository::class -> SqliteSegmentRepository(
             SqliteSegment(
                 Segment.Id(2810),
                 TopicId(1506),
-                createTempDirectory("/tmp/hamal_test/sqlite_topic_test")
+                createTempDirectory("sqlite_topic_test")
             )
         ) as REPO
 
         StateRepository::class -> SqliteStateRepository(
-            SqliteStateRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_state_test"))
+            SqliteStateRepository.Config(createTempDirectory("sqlite_state_test"))
         ) as REPO
 
         TopicRepository::class -> SqliteTopicRepository(
             Topic(TopicId(23), FlowId(23), GroupId(1), TopicName("test-topic")),
-            createTempDirectory("/tmp/hamal_test/sqlite_topic_test")
+            createTempDirectory("sqlite_topic_test")
         ) as REPO
 
         TriggerRepository::class -> SqliteTriggerRepository(
-            SqliteTriggerRepository.Config(createTempDirectory("/tmp/hamal_test/sqlite_trigger_test"))
+            SqliteTriggerRepository.Config(createTempDirectory("sqlite_trigger_test"))
         ) as REPO
 
         else -> TODO()
