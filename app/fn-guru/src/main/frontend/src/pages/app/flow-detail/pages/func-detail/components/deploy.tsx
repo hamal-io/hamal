b28af2cb8b25938b61c59e41268ec9f382280552
Dominique Dymke <dominique@dymke.net>
2023-12-09 00:00:00 +0000

    130 - minor adjustments

--- app/fn-guru/src/main/frontend/src/pages/app/flow-detail/pages/func-detail/components/deploy.tsx
+++ app/fn-guru/src/main/frontend/src/pages/app/flow-detail/pages/func-detail/components/deploy.tsx
@@ -1,40 +1,29 @@
 import * as React from "react";
-import {FC, useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {FC, useState} from "react";
 import {Button} from "@/components/ui/button.tsx";
 import * as z from "zod"
 import {useFuncDeployLatestCode} from "@/hook/func.ts";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
-} from "@/components/ui/form.tsx";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {Dialog, DialogContent, DialogHeader, DialogTrigger} from "@/components/ui/dialog.tsx";
 import {Loader2} from "lucide-react";
 import {useFuncUpdate} from "@/hook";
+import {DialogDescription} from "@radix-ui/react-dialog";
 
 
 const formSchema = z.object({
-    message: z.string().max(50),
+    message: z.string().max(256),
 })
 
 type Props = {
     funcId: string
     code: string;
-    name: string;
 }
 
-const Deploy: FC<Props> = ({funcId, code, name}) => {
-    const navigate = useNavigate()
+const Deploy: FC<Props> = ({funcId, code}) => {
     const [openDialog, setOpenDialog] = useState<boolean>(false)
-    const props = {openModal: openDialog, setOpenModal: setOpenDialog}
     const [isLoading, setLoading] = useState(false)
 
     const [updateFunc, submittedUpdate] = useFuncUpdate()
@@ -50,33 +39,35 @@ const Deploy: FC<Props> = ({funcId, code, name}) => {
     async function onSubmit(values: z.infer<typeof formSchema>) {
         setLoading(true)
         try {
-            updateFunc(funcId, name, code)
-            deployFunc(funcId, values.message)
+            updateFunc(funcId, null, code)
+            setTimeout(() => {
+                deployFunc(funcId, values.message)
+            }, 500)
         } catch (e) {
             console.error(e)
+        } finally {
+            setOpenDialog(false)
+            setLoading(false)
+            form.reset()
         }
     }
-
-    useEffect(() => {
-        if (submittedDeploy !== null) {
-            setOpenDialog(false)
-            form.reset({
-                message: "Deploy message goes here.."
-            })
-        }
-    }, [submittedUpdate, submittedDeploy, navigate]);
 
     return (
         <>
             <Dialog open={openDialog} onOpenChange={setOpenDialog}>
                 <DialogTrigger asChild>
-                    <Button variant="secondary">
+                    <Button>
                         Deploy
                     </Button>
                 </DialogTrigger>
 
                 <DialogContent>
                     <DialogHeader>Function Deployment</DialogHeader>
+
+                    <DialogDescription>
+                        <b> Warning:</b> You are about to replace the code of the function.
+                        It might take a couple of seconds for the system to pick up.
+                    </DialogDescription>
 
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -85,21 +76,27 @@ const Deploy: FC<Props> = ({funcId, code, name}) => {
                                 name="message"
                                 render={({field}) => (
                                     <FormItem>
-                                        <FormLabel></FormLabel>
+                                        <FormLabel>Message</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="Deploy message goes here.." {...field} />
+                                            <Input
+                                                placeholder="What did you do or why do you want to deploy?" {...field} />
                                         </FormControl>
-                                        <FormDescription>
-                                            This might take a second. You can return to an earlier version at any time.
-                                        </FormDescription>
                                         <FormMessage/>
                                     </FormItem>
                                 )}
                             />
-                            <Button type="submit" variant="destructive">
-                                {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin"/>}
-                                Save & Deploy
-                            </Button>
+                            <div className="flex flex-row justify-between items-center">
+                                <Button onClick={() => {
+                                    form.reset()
+                                    setOpenDialog(false)
+                                }} variant="secondary">
+                                    Cancel
+                                </Button>
+                                <Button type="submit" variant="destructive">
+                                    {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin"/>}
+                                    Deploy
+                                </Button>
+                            </div>
                         </form>
                     </Form>
                 </DialogContent>
