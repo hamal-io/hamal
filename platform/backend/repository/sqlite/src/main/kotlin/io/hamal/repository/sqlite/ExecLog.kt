f5217f07908bdf625a424e692dd2f4b4a48435f2
jsilinski <49599523+jonsberg@users.noreply.github.com>
2023-12-11 17:40:40 +0100

    111 impl ExecLogSqliteRepository wip

--- platform/backend/repository/sqlite/src/main/kotlin/io/hamal/repository/sqlite/ExecLog.kt
+++ platform/backend/repository/sqlite/src/main/kotlin/io/hamal/repository/sqlite/ExecLog.kt
@@ -1,52 +1,36 @@
 package io.hamal.repository.sqlite
 
+import io.hamal.lib.domain._enum.ExecLogLevel
+import io.hamal.lib.domain.vo.*
 import io.hamal.lib.sqlite.Connection
+import io.hamal.lib.sqlite.NamedResultSet
 import io.hamal.lib.sqlite.SqliteBaseRepository
 import io.hamal.repository.api.ExecLog
 import io.hamal.repository.api.ExecLogCmdRepository
-import io.hamal.repository.api.ExecLogQueryRepository
+import io.hamal.repository.api.ExecLogQueryRepository.ExecLogQuery
 import io.hamal.repository.api.ExecLogRepository
 import java.nio.file.Path
 
-class ExecLog(
+class ExecLogSqliteRepository(
     config: Config
 ) : SqliteBaseRepository(config), ExecLogRepository {
     data class Config(
         override val path: Path
     ) : SqliteBaseRepository.Config {
-        override val filename = "execlog.db"
-    }
-
-    override fun append(cmd: ExecLogCmdRepository.AppendCmd): ExecLog {
-        TODO("Not yet implemented")
-    }
-
-    override fun list(query: ExecLogQueryRepository.ExecLogQuery): List<ExecLog> {
-        TODO("Not yet implemented")
-    }
-
-    override fun count(query: ExecLogQueryRepository.ExecLogQuery): ULong {
-        TODO("Not yet implemented")
-    }
-
-    override fun clear() {
-        TODO("Not yet implemented")
-    }
-
-    override fun setupConnection(connection: Connection) {
-        TODO("Not yet implemented")
+        override val filename = "exec_log.db"
     }
 
     override fun setupSchema(connection: Connection) {
         connection.tx {
             execute(
                 """
-                CREATE TABLE IF NOT EXISTS auth (
+                CREATE TABLE IF NOT EXISTS exec_log (
                     id INTEGER NOT NULL,
                     exec_id INTEGER NOT NULL,
                     group_id INTEGER NOT NULL,
                     level INTEGER NOT NULL,
-                    timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+                    message TEXT NOT NULL,
+                    timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
                     PRIMARY KEY (id)
                )
             """.trimIndent()
@@ -54,5 +38,125 @@ class ExecLog(
         }
     }
 
+    override fun append(cmd: ExecLogCmdRepository.AppendCmd): ExecLog {
+        return connection.execute<ExecLog>(
+            """
+            INSERT INTO exec_log 
+                (id, exec_id, group_id, message, level)
+            VALUES
+                (:id, :exec_id, :group_id, :message, :level) 
+            RETURNING *;
+        """.trimIndent()
+        ) {
+            query {
+                set("id", cmd.execLogId)
+                set("exec_id", cmd.execId)
+                set("group_id", cmd.groupId)
+                set("message", cmd.message.value)
+                set("level", cmd.level.value)
+            }
+            map(NamedResultSet::toExecLog)
+        }!!
+    }
+
+
+    override fun list(query: ExecLogQuery): List<ExecLog> {
+        return connection.executeQuery<ExecLog>(
+            """
+            SELECT 
+                *
+             FROM
+                exec_log
+            WHERE
+                id < :afterId
+                ${query.groupIds()}
+                ${query.ids()}
+                ${query.execIds()}
+            ORDER BY id DESC
+            LIMIT :limit
+        """.trimIndent()
+        ) {
+            query {
+                set("afterId", query.afterId)
+                set("limit", query.limit)
+            }
+            map(NamedResultSet::toExecLog)
+        }
+    }
+
+    override fun count(query: ExecLogQuery): ULong {
+        return connection.executeQueryOne(
+            """
+            SELECT 
+                COUNT(*) as count 
+            FROM 
+                exec_log
+            WHERE
+                id < :afterId
+                ${query.groupIds()}
+                ${query.ids()}
+                ${query.execIds()}
+            """.trimIndent()
+        ) {
+            query {
+                set("afterId", query.afterId)
+            }
+            map {
+                it.getLong("count").toULong()
+            }
+        } ?: 0UL
+    }
+
+    override fun clear() {
+        connection.tx {
+            execute("DELETE FROM exec_log")
+        }
+    }
+
+
+    override fun setupConnection(connection: Connection) {
+        connection.execute("""PRAGMA journal_mode = wal;""")
+        connection.execute("""PRAGMA locking_mode = exclusive;""")
+        connection.execute("""PRAGMA temp_store = memory;""")
+        connection.execute("""PRAGMA synchronous = off;""")
+    }
+
+
     override fun close() {}
 }
+
+private fun ExecLogQuery.ids(): String {
+    return if (execLogIds.isEmpty()) {
+        ""
+    } else {
+        "AND id IN (${execLogIds.joinToString(",") { "${it.value.value}" }})"
+    }
+}
+
+private fun ExecLogQuery.groupIds(): String {
+    return if (groupIds.isEmpty()) {
+        ""
+    } else {
+        "AND group_id IN (${groupIds.joinToString(",") { "${it.value.value}" }})"
+    }
+}
+
+private fun ExecLogQuery.execIds(): String {
+    return if (execIds.isEmpty()) {
+        ""
+    } else {
+        "AND exec_id IN (${execIds.joinToString(",") { "${it.value.value}" }})"
+    }
+}
+
+private fun NamedResultSet.toExecLog(): ExecLog {
+    return ExecLog(
+        id = getDomainId("id", ::ExecLogId),
+        execId = getDomainId("exec_id", ::ExecId),
+        groupId = getDomainId("group_id", ::GroupId),
+        level = ExecLogLevel.of(getInt("level")),
+        message = ExecLogMessage(getString("message")),
+        timestamp = ExecLogTimestamp(getInstant("timestamp")),
+    )
+}
+
