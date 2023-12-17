6489c104e16c661188d1094d10667644c6cb5b32
Jonathan Silinski <jon.silinski@outlook.de>
2023-12-18 00:20:27 +0100

    134 react table setup

--- app/fn-guru/src/main/frontend/src/pages/app/flow-detail/pages/func-detail/components/history.tsx
+++ app/fn-guru/src/main/frontend/src/pages/app/flow-detail/pages/func-detail/components/history.tsx
@@ -1,73 +1,36 @@
 import React, {FC, useState} from "react";
 import {Button} from "@/components/ui/button"
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
+import {Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger,} from "@/components/ui/dialog"
 import {CounterClockwiseClockIcon} from "@radix-ui/react-icons";
 import {useFuncHistory} from "@/hook";
 import {useNavigate} from "react-router-dom";
-import {Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger} from "@/components/ui/select.tsx";
-import {cn} from "@/utils";
-import {SelectValue} from "@radix-ui/react-select";
+import {Table} from "@/components/ui/table.tsx";
 
 type Props = {
-    flowId: string,
     funcId: string;
 }
-const History: FC<Props> = ({flowId, funcId}) => {
+const History: FC<Props> = ({funcId}) => {
     const navigate = useNavigate()
 
     const [openDialog, setOpenDialog] = useState<boolean>(false)
     const [getHistory, funcHistory, loading] = useFuncHistory()
 
 
-    if (loading) {
-        return "Loading..."
-    }
     return (
-        <Dialog>
+        <Dialog open={openDialog} onOpenChange={setOpenDialog}>
             <DialogTrigger asChild>
-                <Button variant="secondary"  disabled>
+                <Button variant="secondary">
                     <span className="sr-only">Show history</span>
-                    <CounterClockwiseClockIcon className="h-4 w-4" />
+                    <CounterClockwiseClockIcon className="h-4 w-4"/>
                 </Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[800px]">
                 <DialogHeader>
-                    <DialogTitle>History</DialogTitle>
-                    <DialogDescription>
-                        Inspect previous versions of your function
-                    </DialogDescription>
+                    <DialogTitle>Deployed Versions:</DialogTitle>
                 </DialogHeader>
-                {/* <div className="grid gap-4 py-4">
-                    <h1> Show history TBD </h1>
-                </div>*/}
-                <Select value={selected} onValueChange={
-                    (newFuncId) => {
-                        navigate(`/flows/${flowId}/functions/${newFuncId}`, {replace: true})
-                        setSelected(newFuncId)
-                    }
-                }>
-                    <SelectTrigger className={cn("h-8 bg-white", className)}>
-                        <SelectValue/>
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                            <SelectLabel>Versions</SelectLabel>
-                            {funcHistory.versions.map(func =>
-                                <SelectItem key={func.version} value={func.message}>{func.name}</SelectItem>
-                            )}
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-
-
+                <Table>
+                    TODO-134
+                </Table>
                 <DialogFooter/>
             </DialogContent>
         </Dialog>
