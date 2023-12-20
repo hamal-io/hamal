55b1d1c2e2dc311c41a5f8fbe97f4d6c1266bcad
jsilinski <jon.silinski@outlook.de>
2023-12-21 00:35:40 +0100

    142 add feedback repo

---
+++ platform/backend/repository/api/src/main/kotlin/io/hamal/repository/api/Feedback.kt
@@ -0,0 +1,49 @@
+package io.hamal.repository.api
+
+import io.hamal.lib.common.domain.CmdId
+import io.hamal.lib.common.domain.DomainObject
+import io.hamal.lib.common.domain.Limit
+import io.hamal.lib.common.domain.UpdatedAt
+import io.hamal.lib.common.snowflake.SnowflakeId
+import io.hamal.lib.domain._enum.FeedbackMood
+import io.hamal.lib.domain.vo.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Feedback(
+    override val id: FeedbackId,
+    override val updatedAt: UpdatedAt,
+    val mood: FeedbackMood,
+    val message: FeedbackMessage,
+    val email: Email,
+    val accountId: AccountId
+) : DomainObject<FeedbackId>
+
+interface FeedbackRepository : FeedbackCmdRepository, FeedbackQueryRepository
+
+interface FeedbackCmdRepository : CmdRepository {
+    fun create(cmd: CreateCmd)
+
+    data class CreateCmd(
+        val id: CmdId,
+        val mood: FeedbackMood,
+        val message: FeedbackMessage,
+        val email: Email = Email(""),
+        val accountId: AccountId
+    )
+}
+
+interface FeedbackQueryRepository {
+    fun get(feedbackId: FeedbackId) = find(feedbackId)
+        ?: throw NoSuchElementException("Feedback not found")
+
+    fun find(feedbackId: FeedbackId): Feedback?
+    fun list(query: FeedbackQuery): List<Feedback>
+    fun count(query: FeedbackQuery)
+
+    data class FeedbackQuery(
+        var afterId: CodeId = CodeId(SnowflakeId(Long.MAX_VALUE)),
+        var limit: Limit = Limit(1),
+        var feedbackIds: List<FeedbackId> = listOf()
+    )
+}
---
+++ platform/lib/domain/src/main/kotlin/io/hamal/lib/domain/_enum/FeedbackMood.kt
@@ -0,0 +1,9 @@
+package io.hamal.lib.domain._enum
+
+enum class FeedbackMood {
+    Happy,
+    Normal,
+    Angry,
+    Disappointed,
+    Frustrated
+}
