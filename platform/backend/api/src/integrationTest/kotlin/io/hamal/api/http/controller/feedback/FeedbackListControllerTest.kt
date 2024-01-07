f67857a7b750988fe0f4a45fb8553f450f72405d
jonsberg <jon.silinski@outlook.de>
2024-01-07 01:06:14 +0100

    142 feedback list test

--- platform/backend/api/src/integrationTest/kotlin/io/hamal/api/http/controller/feedback/FeedbackListControllerTest.kt
+++ platform/backend/api/src/integrationTest/kotlin/io/hamal/api/http/controller/feedback/FeedbackListControllerTest.kt
@@ -1,8 +1,12 @@
 package io.hamal.api.http.controller.feedback
 
+import io.hamal.lib.domain._enum.FeedbackMood
+import io.hamal.lib.domain.request.FeedbackCreateRequest
+import io.hamal.lib.domain.vo.FeedbackMessage
+import io.hamal.repository.api.FeedbackCmdRepository
 import io.hamal.repository.api.FeedbackList
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.empty
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 
 internal class FeedbackListControllerTest : FeedbackBaseControllerTest() {
@@ -12,5 +16,52 @@ internal class FeedbackListControllerTest : FeedbackBaseControllerTest() {
             .execute(FeedbackList::class)
 
         assertThat(res.feedbacks, empty())
+    }
+
+    @Test
+    fun `Single Feedback`() {
+        val fbId = createFeedback(
+            FeedbackCreateRequest(
+                mood = FeedbackMood.Normal,
+                message = FeedbackMessage("My mood is so normal"),
+                accountId = null
+            )
+        ).feedbackId
+
+        val list = httpTemplate.get("/v1/feedbacks")
+            .execute(FeedbackList::class)
+
+        with(list.feedbacks[0]) {
+            assertThat(mood, equalTo(FeedbackMood.Normal))
+            assertThat(message, equalTo(FeedbackMessage("My mood is so normal")))
+            assertThat(accountId, nullValue())
+        }
+    }
+
+    @Test
+    fun `Limit and skip feedbacks`() {
+        val requests = IntRange(0, 20).map {
+            createFeedback(
+                FeedbackCreateRequest(
+                    mood = FeedbackMood.Normal,
+                    message = FeedbackMessage("This is message number $it"),
+                    accountId = null
+                )
+            )
+        }
+
+        val ten = requests[10]
+
+        val list = httpTemplate.get("/v1/feedbacks")
+            .parameter("after_id", ten.feedbackId)
+            .parameter("limit", 1)
+            .execute(FeedbackList::class)
+
+        assertThat(list.feedbacks, hasSize(1))
+        with(list.feedbacks.first()) {
+            assertThat(mood, equalTo(FeedbackMood.Normal))
+            assertThat(message, equalTo(FeedbackMessage("This is message number 9")))
+            assertThat(accountId, nullValue())
+        }
     }
 }
