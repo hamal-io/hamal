1030d34ada12024bf90336a32df6dea924733a7a
jsilinski <jon.silinski@outlook.de>
2024-01-11 00:51:39 +0100

    142 ui feedback button

--- app/fn-guru/src/main/frontend/src/components/app/feedback.tsx
+++ app/fn-guru/src/main/frontend/src/components/app/feedback.tsx
@@ -1,14 +1,48 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import * as z from "zod";
 import {FeedbackMood} from "@/types/feedback.ts";
-
-const formSchema = z.object({
-    name: z.string(),
-})
+import {Dialog, DialogContent, DialogHeader, DialogTrigger} from "@/components/ui/dialog.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {Form} from "@/components/ui/form.tsx";
+import {useFeedbackCreate} from "@/hook/feedback.ts";
 
 const Feedback = () => {
     const [openDialog, setOpenDialog] = useState<boolean>(false)
-    async function onSubmit(message: z.infer<typeof formSchema>, mood: FeedbackMood)
+    const [isLoading, setLoading] = useState(false)
+    const [create, submitted] = useFeedbackCreate()
 
-    //TODO-142
+    async function onSubmit(message: string, mood: FeedbackMood) {
+        setLoading(true)
+        try {
+            create(message, mood)
+        } catch (e) {
+            console.error(e)
+        }
+        setLoading(false)
+    }
+
+    return (
+        <div style={{
+            position: 'fixed',
+            top: '50%',
+            right: 0,
+            transform: 'translateY(-50%) rotate(90deg)',
+            cursor: 'pointer',
+        }}>
+            <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+                <DialogTrigger asChild>
+                    <Button variant="secondary" size="lg">
+                        Feedback
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>Leave Feedback</DialogHeader>
+                    <Button type="submit"></Button>
+
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
 }
+
+export default Feedback
--- app/fn-guru/src/main/frontend/src/hook/feedback.ts
+++ app/fn-guru/src/main/frontend/src/hook/feedback.ts
@@ -3,16 +3,17 @@ import {usePost} from "@/hook/http.ts";
 import {useCallback} from "react";
 import {useAuth} from "@/hook/auth.ts";
 
-type FeedbackCreateAction = (message: string, mood: FeedbackMood, anon: boolean, abortController?: AbortController) => void
+type FeedbackCreateAction = (message: string, mood: FeedbackMood, abortController?: AbortController) => void
 export const useFeedbackCreate = (): [FeedbackCreateAction, FeedbackCreateSubmitted, boolean, Error] => {
     const [auth] = useAuth()
+    const id = auth.accountId
     const [post, submission, loading, error] = usePost<FeedbackCreateSubmitted>()
-    const fn = useCallback(async (message: string, mood: FeedbackMood, anon: boolean , abortController?: AbortController) =>
+    const fn = useCallback(async (message: string, mood: FeedbackMood, abortController?: AbortController) =>
         post('/v1/feedback/create',
             {
                 message,
                 mood,
-                //anon ? auth.accountId : null
+                id
             },
             abortController), [auth]
     )
