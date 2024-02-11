f7d7c1daf188d892ba3b12014ea6d1b4f9cda125
Dominique Dymke <dominique@dymke.net>
2024-02-11 00:00:00 +0000

    183 - schedules page

--- app/fn-guru/src/main/frontend/src/pages/app/schedule-list/index.tsx
+++ app/fn-guru/src/main/frontend/src/pages/app/schedule-list/index.tsx
@@ -1,11 +1,39 @@
-import React, {FC, useContext} from "react";
-import List from "@/pages/app/schedule-list/components/list";
+import React, {FC, useContext, useEffect} from "react";
 import {GroupLayoutContext} from "@/components/app/layout";
+import {useTriggerListSchedule} from "@/hook";
+import {PageHeader} from "@/components/page-header.tsx";
+import {columns} from "@/pages/app/schedule-list/components/columns.tsx";
+import Table from "@/pages/app/schedule-list/components/table.tsx";
+import CreateFixedRate from "@/pages/app/schedule-list/components/create.tsx";
 
 type Props = {}
 const ScheduleListPage: FC<Props> = () => {
     const {groupId, groupName} = useContext(GroupLayoutContext)
-    return (<List group={{id: groupId, name: groupName}}/>)
+    const [listSchedules, scheduleList, loading, error] = useTriggerListSchedule()
+
+    useEffect(() => {
+        const abortController = new AbortController()
+        listSchedules(groupId, abortController)
+        return () => {
+            abortController.abort()
+        }
+    }, [groupId]);
+
+    if (loading) return "Loading..."
+    if (error != null) return "Error -"
+
+    return (
+        <div className="pt-2 px-2 mb-6">
+            <PageHeader
+                title="Schedules"
+                description={`Schedules TBD`}
+                actions={[
+                    <CreateFixedRate/>
+                ]}
+            />
+            <Table data={scheduleList.triggers} columns={columns}/>
+        </div>
+    );
 }
 
 export default ScheduleListPage
