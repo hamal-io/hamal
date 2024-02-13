c67e21f3d69ab7aef79f099b5d1631a1fee49206
jsilinski <jon.silinski@outlook.de>
2024-02-13 22:29:05 +0100

    196 cleanup

--- app/fn-guru/src/main/frontend/src/hook/blueprint.ts
+++ app/fn-guru/src/main/frontend/src/hook/blueprint.ts
@@ -12,11 +12,11 @@ export const useBlueprintGet = (): [BlueprintGetAction, Blueprint, boolean, Erro
     return [fn, bp, loading, error]
 }
 
-type BlueprintListAction = (namespaceId: string, abortController?: AbortController) => void
+type BlueprintListAction = (groupId: string, abortController?: AbortController) => void
 export const useFuncList = (): [BlueprintListAction, BlueprintList, boolean, Error] => {
     const [auth] = useAuth()
     const [get, bps, loading, error] = useGet<BlueprintList>()
-    const fn = useCallback(async (namespaceId: string, abortController?: AbortController) =>
-        get(`/v1/namespaces/${namespaceId}/blueprints`, abortController), [auth])
+    const fn = useCallback(async (groupId: string, abortController?: AbortController) =>
+        get(`/v1/groups/${groupId}/blueprints`, abortController), [auth])
     return [fn, bps, loading, error]
 }
--- platform/backend/api/src/main/kotlin/io/hamal/api/http/controller/blueprint/BlueprintListController.kt
+++ platform/backend/api/src/main/kotlin/io/hamal/api/http/controller/blueprint/BlueprintListController.kt
@@ -2,11 +2,9 @@ package io.hamal.api.http.controller.blueprint
 
 import io.hamal.core.adapter.BlueprintListPort
 import io.hamal.core.component.Retry
-import io.hamal.lib.common.domain.Limit
-import io.hamal.lib.domain.vo.ExtensionId
 import io.hamal.lib.domain.vo.GroupId
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,11 +13,9 @@ class BlueprintListController(
     private val getBlueprint: BlueprintListPort
 ) {
 
-    @GetMapping("/v1/blueprints")
+    @GetMapping("/v1/groups/{groupId}/blueprints")
     fun listBlueprints(
-        @RequestParam(required = false, name = "after_id", defaultValue = "7FFFFFFFFFFFFFFF") afterId: ExtensionId,
-        @RequestParam(required = false, name = "limit", defaultValue = "100") limit: Limit,
-        @RequestParam(required = false, name = "group_ids", defaultValue = "") groupIds: List<GroupId>
+        @PathVariable("groupId") groupId: GroupId
     ) {
     }/*: ResponseEntity<ApiBlueprintList> {
         return ResponseEntity.ok(ApiBlueprintList(
