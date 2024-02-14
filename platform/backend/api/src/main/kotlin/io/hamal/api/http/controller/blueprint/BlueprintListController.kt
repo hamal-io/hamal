1871de8c9d5ca06ab5f2cf084de1841513fa4c1a
jsilinski <jon.silinski@outlook.de>
2024-02-14 16:45:53 +0100

    196 BlueprintListController Test

--- platform/backend/api/src/main/kotlin/io/hamal/api/http/controller/blueprint/BlueprintListController.kt
+++ platform/backend/api/src/main/kotlin/io/hamal/api/http/controller/blueprint/BlueprintListController.kt
@@ -1,6 +1,7 @@
 package io.hamal.api.http.controller.blueprint
 
 import io.hamal.core.adapter.BlueprintListPort
+import io.hamal.core.component.Retry
 import io.hamal.lib.common.domain.Limit
 import io.hamal.lib.domain.vo.BlueprintId
 import io.hamal.lib.sdk.api.ApiBlueprintList
@@ -12,8 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BlueprintListController(
-    private val listBlueprints: BlueprintListPort
+internal class BlueprintListController(
+    private val listBlueprints: BlueprintListPort,
+    private val retry: Retry,
 ) {
 
     @GetMapping("/v1/blueprints")
@@ -21,8 +23,8 @@ class BlueprintListController(
         @RequestParam(required = false, name = "after_id", defaultValue = "7FFFFFFFFFFFFFFF") bpId: BlueprintId,
         @RequestParam(required = false, name = "limit", defaultValue = "10") limit: Limit,
         @RequestParam(required = false, name = "blueprint_ids", defaultValue = "") bpIds: List<BlueprintId>,
-    ): ResponseEntity<ApiBlueprintList> {
-        return listBlueprints(
+    ): ResponseEntity<ApiBlueprintList> = retry {
+        listBlueprints(
             BlueprintQuery(
                 afterId = bpId,
                 limit = limit,
