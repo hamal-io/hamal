14b93fdb2c09fc5d8235d42f45200155495f95e1
Dominique Dymke <dominique@dymke.net>
2024-03-10 00:00:00 +0000

    239 - brings back controller not found test

--- platform/testbed/src/integrationTest/resources/api/misc/controller_not_found/01.lua
+++ platform/testbed/src/integrationTest/resources/api/misc/controller_not_found/01.lua
@@ -1,17 +1,17 @@
---http = require('net.http').create({})
---debug = require_plugin 'debug'
---
---res = fail_on_error(http.post({
---    url = context.env.api_host .. '/v1/anonymous-accounts',
---    json = { }
---}))
---
-----debug.sleep(100)
-----
-----err, res = http.get({ url = context.env.api_host .. '/v1/does/not/exists', headers = {
-----    ['authorization'] = 'Bearer ' .. res.content.token
-----} })
-----
-----assert(err == nil)
-----assert(res.status_code == 404)
-----assert(res.content.message == 'Request handler not found')
+http = require('net.http').create({})
+debug = require_plugin 'debug'
+
+res = fail_on_error(http.post({
+    url = context.env.api_host .. '/v1/anonymous-accounts',
+    json = { }
+}))
+
+debug.sleep(100)
+
+err, res = http.get({ url = context.env.api_host .. '/v1/does/not/exists', headers = {
+    ['authorization'] = 'Bearer ' .. res.content.token
+} })
+
+assert(err == nil)
+assert(res.status_code == 404)
+assert(res.content.message == 'Request handler not found')
