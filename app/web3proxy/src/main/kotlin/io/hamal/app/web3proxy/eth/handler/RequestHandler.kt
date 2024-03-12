85046da0412c18b1e4fee6122ec590f445ebb0dd
Dominique Dymke <dominique@dymke.net>
2024-03-12 00:00:00 +0000

    264 - small clean up

--- app/web3proxy/src/main/kotlin/io/hamal/app/web3proxy/eth/handler/RequestHandler.kt
+++ app/web3proxy/src/main/kotlin/io/hamal/app/web3proxy/eth/handler/RequestHandler.kt
@@ -1,12 +1,14 @@
 package io.hamal.app.web3proxy.eth.handler
 
 import io.hamal.app.web3proxy.eth.repository.EthBlockDataRepository
-import io.hamal.lib.web3.eth.domain.*
+import io.hamal.lib.web3.eth.domain.EthGetBlockByNumberRequest
+import io.hamal.lib.web3.eth.domain.EthGetBlockResponse
+import io.hamal.lib.web3.eth.domain.EthRequest
+import io.hamal.lib.web3.eth.domain.EthResponse
 
 interface HandleEthRequest {
     operator fun invoke(request: EthRequest): EthResponse
     operator fun invoke(requests: List<EthRequest>): List<EthResponse>
-
 }
 
 class EthRequestHandlerImpl(
@@ -19,54 +21,15 @@ class EthRequestHandlerImpl(
     }
 
     override fun invoke(requests: List<EthRequest>): List<EthResponse> {
-//        val template = HttpTemplateImpl("")
-
-
         val blocks = requests.filterIsInstance<EthGetBlockByNumberRequest>().map { it.number }
+        val getBlockNumberRequestIds = requests.filterIsInstance<EthGetBlockByNumberRequest>()
+            .associate { it.number to it.id }
 
         return blockDataRepository.list(blocks).map { block ->
             EthGetBlockResponse(
-                id = EthRequestId(1),
+                id = getBlockNumberRequestIds[block.number]!!,
                 result = block
             )
         }
-
-//        val requestedBlocksByNumbers = requests.filterIsInstance<HotObject>()
-//            .filter { it.stringValue("method") == "eth_getBlockByNumber" }
-//            .map { it.asArray("params") }
-//            .map { params ->
-//                val fullBlock = if (params.isBoolean(1)) {
-//                    params.booleanValue(1)
-//                } else {
-//                    true
-//                }
-//                EthUint64(EthPrefixedHexString(params[0].stringValue)) to fullBlock
-//            }
-
-//        val blocks = blockDataRepository.list(requestedBlocksByNumbers.map { it.first })
-
-        // map full or partial representation
-
-
-//        blockDataRepository.
-
-
-//        val response = template
-//            .post("https://rpc.ankr.com/eth/dc1bb7e07d13bf1a2fe5e8f1662664c39abc5413f6f96e1fb6145ed3abe06842")
-//            .body(
-//                HotArray.builder()
-//                    .append(
-//                        HotObject.builder()
-//                            .set("id", "1")
-//                            .set("jsonrpc", "2.0")
-//                            .set("method", "eth_getBlockByNumber")
-//                            .set("params", HotArray.builder().append("0x1").append(true).build())
-//                            .build()
-//                    ).build()
-//            )
-//            .execute(HotArray::class)
-
-
-//        return listOf()
     }
 }
