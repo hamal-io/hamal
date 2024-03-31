e2740c67fa70741601bc8a4df2afa3195a288e3a
Dominique Dymke <dominique@dymke.net>
2024-03-31 00:00:00 +0000

    Removes a lot of clutter

--- app/nyanbot/src/main/frontend/src/hooks/template/usePathname.ts
+++
@@ -1,9 +0,0 @@
-import { useLocation } from 'react-router-dom';
-
-//Wrapper for next.js
-const usePathnameW = () => {
-    const location = useLocation();
-    return location.pathname;
-};
-
-export default usePathnameW;
