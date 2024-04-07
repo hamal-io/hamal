8fd00e575ec15cce2db07b6a3dd536ee6fa0884d
Dominique Dymke <dominique@dymke.net>
2024-04-07 00:00:00 +0000

    Account Get Me Route

--- app/nyanbot/src/main/kotlin/com/nyanbot/http/controller/adhoc/AdhocController.kt
+++ app/nyanbot/src/main/kotlin/com/nyanbot/http/controller/adhoc/AdhocController.kt
@@ -1,15 +1,8 @@
 package com.nyanbot.http.controller.adhoc
 
-import com.nyanbot.json
-import io.hamal.lib.domain._enum.CodeType
+import com.nyanbot.component.Hamal
 import io.hamal.lib.domain.vo.CodeValue
 import io.hamal.lib.domain.vo.InvocationInputs
-import io.hamal.lib.domain.vo.NamespaceId
-import io.hamal.lib.http.HttpTemplateImpl
-import io.hamal.lib.nodes.Graph
-import io.hamal.lib.sdk.ApiSdkImpl
-import io.hamal.lib.sdk.api.ApiAdhocInvokeRequest
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -22,35 +15,15 @@ data class AdhocInvokeRequest(
 
 @RestController
 internal class AdhocController(
-    @Value("\${io.hamal.server.url}") val url: String,
-    @Value("\${io.hamal.server.token}") val token: String,
+    private val hamal: Hamal
 ) {
 
     @PostMapping("/v1/adhoc")
     fun invoke(
         @RequestBody req: AdhocInvokeRequest
     ) {
-        println("About to send ${req.code.value}")
+        hamal.setup()
 
-        val sdk = ApiSdkImpl(HttpTemplateImpl(
-            baseUrl = url,
-            headerFactory = { this["Authorization"] = "Bearer $token" }
-        ))
-
-        sdk.adhoc(
-            NamespaceId.root, ApiAdhocInvokeRequest(
-                inputs = req.inputs,
-                code = CodeValue(
-                    json.serialize(
-                        Graph(
-                            nodes = listOf(),
-                            connections = listOf()
-                        )
-                    )
-                ),
-                codeType = CodeType.Nodes
-            )
-        )
     }
 
 }
