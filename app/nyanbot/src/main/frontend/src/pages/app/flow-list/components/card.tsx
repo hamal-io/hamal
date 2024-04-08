de7dc418f655f3a08d63d4f386a040da87e5c74f
jsilinski <jon.silinski@outlook.de>
2024-04-08 17:06:49 +0200

    293 add a status switch

--- app/nyanbot/src/main/frontend/src/pages/app/flow-list/components/card.tsx
+++ app/nyanbot/src/main/frontend/src/pages/app/flow-list/components/card.tsx
@@ -1,37 +1,32 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 import {Switch} from "@/components/ui/switch.tsx";
+
 
 type Props = {
     flow: Flow
 }
 export const FlowCard: FC<Props> = ({flow}) => {
     const navigate = useNavigate()
+    const [active, setActive] = useState<boolean>(false)
 
     const {id, name, status} = flow
 
-    function handleClick(e: React.MouseEvent) {
-        e.stopPropagation()
-        e.preventDefault()
+
+    function handleClick() {
         navigate(`/flows/${id}`)
     }
 
-    function handleCheck(e) {
-        e.stopPropagation()
-        e.preventDefault()
+    function handleCheck() {
+        setActive(!active)
     }
 
-    const active = status === "Active"
 
     return (
-        <Card onClick={handleClick}>
-            <CardHeader>
-                <CardTitle>{name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <Switch checked={active} className={"float-right"} onCheckedChange={handleCheck}></Switch>
-            </CardContent>
+        <Card onClick={handleClick} className={"flex flex-row items-center justify-between p-4"}>
+            <CardTitle className={""}>{name}</CardTitle>
+            <Switch checked={active} onCheckedChange={handleCheck} onClick={(e) => e.stopPropagation()}></Switch>
         </Card>
     )
 }
