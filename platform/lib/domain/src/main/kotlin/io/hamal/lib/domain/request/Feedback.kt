8865bc20ef33a1f884de63a1afbb92ca78fcf6d5
jsilinski <jon.silinski@outlook.de>
2024-05-06 09:02:35 +0200

    321 rework

--- platform/lib/domain/src/main/kotlin/io/hamal/lib/domain/request/Feedback.kt
+++ platform/lib/domain/src/main/kotlin/io/hamal/lib/domain/request/Feedback.kt
@@ -7,35 +7,7 @@ data class FeedbackCreateRequest(
     val mood: FeedbackMood,
     val message: FeedbackMessage,
     val accountId: AccountId?
-) {
-    /*object Adapter : AdapterJson<FeedbackCreateRequest> {
-        override fun serialize(
-            src: FeedbackCreateRequest,
-            typeOfSrc: Type,
-            context: JsonSerializationContext
-        ): JsonElement {
-            val obj = JsonObject()
-            obj.addProperty("mood", src.mood.value)
-            obj.addProperty("message", src.message.value.stringValue)
-            if (src.accountId != null) {
-                obj.addProperty("accountId", src.accountId.stringValue)
-            }
-            return obj
-        }
-
-        override fun deserialize(
-            json: JsonElement,
-            typeOfT: Type,
-            context: JsonDeserializationContext
-        ): FeedbackCreateRequest {
-            val obj = json.asJsonObject
-            val mood = FeedbackMoods.of(obj.get("mood").asInt)
-            val message = FeedbackMessage(obj.get("message").asString)
-            val accountId = obj.get("accountId")?.let { AccountId(it.asString) }
-            return FeedbackCreateRequest(mood, message, accountId)
-        }
-    }*/
-}
+)
 
 data class FeedbackCreateRequested(
     override val requestId: RequestId,
