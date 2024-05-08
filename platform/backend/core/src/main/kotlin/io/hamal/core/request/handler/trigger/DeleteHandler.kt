fa93ee925fed67a47f5d0422f984af06b19e9a27
jsilinski <jon.silinski@outlook.de>
2024-05-08 08:59:30 +0200

    336 handler fix

--- platform/backend/core/src/main/kotlin/io/hamal/core/request/handler/trigger/DeleteHandler.kt
+++ platform/backend/core/src/main/kotlin/io/hamal/core/request/handler/trigger/DeleteHandler.kt
@@ -1,6 +1,5 @@
 package io.hamal.core.request.handler.trigger
 
-import io.hamal.core.adapter.trigger.TriggerGetPort
 import io.hamal.core.event.InternalEventEmitter
 import io.hamal.core.request.RequestHandler
 import io.hamal.core.request.handler.cmdId
@@ -8,18 +7,19 @@ import io.hamal.lib.common.domain.CmdId
 import io.hamal.lib.domain.request.TriggerDeleteRequested
 import io.hamal.repository.api.Trigger
 import io.hamal.repository.api.TriggerCmdRepository
+import io.hamal.repository.api.TriggerQueryRepository
 import io.hamal.repository.api.event.TriggerDeletedEvent
 import org.springframework.stereotype.Component
 
 @Component
 internal class TriggerDeleteHandler(
     private val triggerCmdRepository: TriggerCmdRepository,
-    private val triggerGetPort: TriggerGetPort,
+    private val triggerQueryRepository: TriggerQueryRepository,
     private val eventEmitter: InternalEventEmitter
 ) : RequestHandler<TriggerDeleteRequested>(TriggerDeleteRequested::class) {
 
     override fun invoke(req: TriggerDeleteRequested) {
-        val trigger = triggerGetPort(req.id)
+        val trigger = triggerQueryRepository.get(req.id)
 
         triggerCmdRepository.delete(
             TriggerCmdRepository.DeleteCmd(
