0e4222b9a11be87342d2d8694fe0699e7873610c
jsilinski <jon.silinski@outlook.de>
2024-05-09 21:20:42 +0200

    341-3 exec.get, namespace.append, func.list + tests

--- platform/testbed/src/integrationTest/resources/api/func/list/by_namespace/01.lua
+++ platform/testbed/src/integrationTest/resources/api/func/list/by_namespace/01.lua
@@ -1,23 +1,25 @@
-sys = require_plugin('std.sys')
+sys = require('std.sys').create({
+    base_url = context.env.api_host
+})
 
-namespace_one_req = fail_on_error(sys.namespaces.append({ name = 'namespace-one' }))
+namespace_one_req = fail_on_error(sys.namespace.append({ name = 'namespace-one' }))
 sys.await_completed(namespace_one_req)
 
-namespace_two_req = fail_on_error(sys.namespaces.append({ name = 'namespace-two' }))
+namespace_two_req = fail_on_error(sys.namespace.append({ name = 'namespace-two' }))
 sys.await_completed(namespace_two_req)
 
 sys.await_completed(
-        fail_on_error(sys.funcs.create({ namespace_id = namespace_one_req.id, name = 'func-1' }))
+        fail_on_error(sys.func.create({ namespace_id = namespace_one_req.id, name = 'func-1' }))
 )
 
-count = #fail_on_error(sys.funcs.list())
+count = #fail_on_error(sys.func.list())
 assert(count == 1)
 
-count = #fail_on_error(sys.funcs.list({}))
+count = #fail_on_error(sys.func.list({}))
 assert(count == 1)
 
-count = #fail_on_error(sys.funcs.list({ namespace_ids = { namespace_one_req.id } }))
+count = #fail_on_error(sys.func.list({ namespace_ids = { namespace_one_req.id } }))
 assert(count == 1)
 
-count = #fail_on_error(sys.funcs.list({ namespace_ids = { namespace_two_req.id } }))
+count = #fail_on_error(sys.func.list({ namespace_ids = { namespace_two_req.id } }))
 assert(count == 0)
--- platform/testbed/src/integrationTest/resources/api/func/list/by_nested_namespace/01.lua
+++ platform/testbed/src/integrationTest/resources/api/func/list/by_nested_namespace/01.lua
@@ -1,19 +1,31 @@
-sys = require_plugin('std.sys')
+sys = require('std.sys').create({
+    base_url = context.env.api_host
+})
 
-namespace_one_req = fail_on_error(sys.namespaces.append({ name = 'namespace-one' }))
+namespace_one_req = fail_on_error(sys.namespace.append({ name = 'namespace-one' }))
 sys.await_completed(namespace_one_req)
 
-namespace_two_req = fail_on_error(sys.namespaces.append({ name = 'namespace-two' }))
+namespace_two_req = fail_on_error(sys.namespace.append({ name = 'namespace-two' }))
 sys.await_completed(namespace_two_req)
 
-sys.await_completed(fail_on_error(sys.funcs.create({ namespace_id = namespace_one_req.id, name = 'func-1' })))
-sys.await_completed(fail_on_error(sys.funcs.create({ namespace_id = namespace_two_req.id, name = 'func-2' })))
+sys.await_completed(
+        fail_on_error(sys.func.create(
+                { namespace_id = namespace_one_req.id, name = 'func-1' })
+        )
+)
 
-assert(#fail_on_error(sys.funcs.list({ namespace_ids = { namespace_one_req.id } })) == 1)
-assert(#fail_on_error(sys.funcs.list({ namespace_ids = { namespace_two_req.id } })) == 1)
+sys.await_completed(
+        fail_on_error(sys.func.create(
+                { namespace_id = namespace_two_req.id, name = 'func-2' }))
+)
 
--- root-namespace contains 2 namespace-one (func-1) and namespace_two(func-2)
-root_namespace_hooks = fail_on_error(sys.funcs.list({ namespace_ids = { '539' } }))
+func_list = fail_on_error(sys.func.list({ namespace_ids = { namespace_one_req.id } }))
+
+assert(#fail_on_error(sys.func.list({ namespace_ids = { namespace_one_req.id } })) == 1)
+assert(#fail_on_error(sys.func.list({ namespace_ids = { namespace_two_req.id } })) == 1)
+
+--root-namespace contains 2 namespace-one (func-1) and namespace_two(func-2)
+root_namespace_hooks = fail_on_error(sys.func.list({ namespace_ids = { '539' } }))
 assert(#root_namespace_hooks == 2)
 assert(root_namespace_hooks[1].name == 'func-2')
 assert(root_namespace_hooks[2].name == 'func-1')
