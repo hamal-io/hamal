2e1167ee50a48f88591de3ebf2244e996abbde21
jsilinski <49599523+jonsberg@users.noreply.github.com>
2024-05-12 11:43:33 +0200

    341-1 NamespaceId & WorkspaceId available in context (#342)
    Co-authored-by: ddymke <77837794+dominique-dymke@users.noreply.github.com>

---
+++ platform/runner/runner/src/integrationTest/kotlin/io/hamal/runner/run/NamespaceIdTest.kt
@@ -0,0 +1,69 @@
+package io.hamal.runner.run
+
+import io.hamal.lib.common.value.ValueString
+import io.hamal.lib.domain.State
+import io.hamal.lib.domain._enum.CodeTypes.Lua54
+import io.hamal.lib.domain.vo.CodeType.Companion.CodeType
+import io.hamal.lib.domain.vo.CodeValue.Companion.CodeValue
+import io.hamal.lib.domain.vo.ExecId.Companion.ExecId
+import io.hamal.lib.domain.vo.ExecInputs
+import io.hamal.lib.domain.vo.ExecToken.Companion.ExecToken
+import io.hamal.lib.domain.vo.NamespaceId
+import io.hamal.lib.domain.vo.NamespaceId.Companion.NamespaceId
+import io.hamal.lib.domain.vo.TriggerId.Companion.TriggerId
+import io.hamal.lib.domain.vo.WorkspaceId.Companion.WorkspaceId
+import io.hamal.lib.kua.function.Function0In0Out
+import io.hamal.lib.kua.function.FunctionContext
+import io.hamal.runner.connector.UnitOfWork
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.Test
+
+internal class NamespaceIdTest : AbstractTest() {
+    @Test
+    fun `NamespaceId available in code`() {
+        runTest(
+            UnitOfWork(
+                id = ExecId(1234),
+                execToken = ExecToken("ExecToken"),
+                namespaceId = NamespaceId(9876),
+                workspaceId = WorkspaceId(5432),
+                triggerId = TriggerId(4567),
+                inputs = ExecInputs(),
+                state = State(),
+                code = CodeValue("assert(context.exec.namespace_id == '2694')"),
+                codeType = CodeType(Lua54),
+                correlation = null
+            )
+        )
+    }
+
+    @Test
+    fun `Namespace id available in function`() {
+        val testFn = TestFunction()
+
+        runTest(
+            UnitOfWork(
+                id = ExecId(1234),
+                execToken = ExecToken("ExecToken"),
+                namespaceId = NamespaceId(9876),
+                workspaceId = WorkspaceId(5432),
+                triggerId = TriggerId(4567),
+                inputs = ExecInputs(),
+                state = State(),
+                code = CodeValue("require_plugin('test').fn()"),
+                codeType = CodeType(Lua54),
+                correlation = null
+            ),
+            testPlugins = mapOf(ValueString("fn") to testFn)
+        )
+
+        assertThat(testFn.result, equalTo("2694"))
+    }
+
+    class TestFunction(var result: String? = null) : Function0In0Out() {
+        override fun invoke(ctx: FunctionContext) {
+            result = ctx[NamespaceId::class].stringValue
+        }
+    }
+}
