171d90217a0e231da08d0baff3e91de6a6f1466c
jsilinski <jon.silinski@outlook.de>
2024-05-22 23:37:14 +0200

    355 react flow editor tryout

--- app/nyanbot/src/pages/app/nodes-editor/index.tsx
+++ app/nyanbot/src/pages/app/nodes-editor/index.tsx
@@ -1,29 +1,39 @@
 import {History, LucideIcon, Menu, Play, Plus, Save, Workflow} from 'lucide-react';
 import React, {FC} from "react";
 import {Button} from "@/components/ui/button.tsx";
+import ReactFlow, {Background, Controls} from 'reactflow';
+import 'reactflow/dist/style.css';
 
 
 const NodesEditorPage = () => {
     return (
-        <main className={"flex-col pt-2"}>
-            <div className={"p-4"}>
-                <div className={"flex flex-row p-2 border rounded-md justify-between h-18 max-w-7xl"}>
-                    <div>
-                        <MenuItem icon={Menu}/>
-                    </div>
-                    <div className={"flex flex-row justify-between gap-4"}>
-                        <MenuItem icon={Save}/>
-                        <MenuItem icon={Play}/>
-                    </div>
-
+        <main className={"p-2 h-full"}>
+            <ReactFlow>
+                <Background/>
+                <div className={"absolute bottom-12 right-16 z-50"}>
+                    <Controls/>
                 </div>
-                <div className={"flex flex-col mt-12 p-2 w-16 items-center justify-evenly  gap-4 "}>
-                    <MenuItem icon={Plus}/>
-                    <MenuItem icon={Workflow}/>
-                    <MenuItem icon={History}/>
+                <div className={"absolute inset-x-2 top-4"}>
+                    <div
+                        className={"flex flex-row p-2  h-18 border rounded-md justify-between bg-white items-center max-w-7xl z-50"}>
+                        <div>
+                            <MenuItem icon={Menu}/>
+                        </div>
+                        <div className={"flex flex-row justify-between gap-4"}>
+                            <MenuItem icon={Save}/>
+                            <MenuItem icon={Play}/>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className={"absolute bottom-0 left-0 border-2 h-12 w-screen max-w-7xl"}>
+                <div className={"absolute inset-x-2 inset-y-14 w-16 h-16"}>
+                    <div className={"flex flex-col mt-12 p-2 w-16  items-center justify-evenly gap-4 z-50"}>
+                        <MenuItem icon={Plus}/>
+                        <MenuItem icon={Workflow}/>
+                        <MenuItem icon={History}/>
+                    </div>
+                </div>
+            </ReactFlow>
+            <div className={"absolute bottom-0 left-0 border-2 h-12 w-screen bg-white max-w-7xl"}>
                 <div className={"flex flex-row gap-4 justify-items-start"}>
                     <div>
                         Flow 1
@@ -45,7 +55,7 @@ type ButtonProps = {
 }
 const MenuItem: FC<ButtonProps> = ({icon: Icon}) => {
     return (
-        <Button className={"w-14 h-14"}>
+        <Button className={"w-14 h-14 bg-white"}>
             <Icon size={24}/>
         </Button>
     )
